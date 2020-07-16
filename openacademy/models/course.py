bf672301e2d7d08aecbf6aaec66d929f4bd60cbd
Nelson Díaz <diaznelson23@gmail.com>
2020-07-15 18:31:11 -0600

    [FIX] attendees id

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -12,7 +12,7 @@ class Course(models.Model):
         ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -26,3 +26,4 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
